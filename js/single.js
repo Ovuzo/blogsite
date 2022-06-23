a2f9a52a57a1512e9a92c12361bd02e160f2c72d
Ovuzo <ovuzonyobeno@gmail.com>
2022-06-23 08:50:00 +0100

    updated

--- js/single.js
+++ js/single.js
@@ -4,18 +4,8 @@ function renderSingle() {
     console.log(post)
 
     document.getElementById('post-id').innerHTML = post.id
-    document.getElementById('post-title').innerHTML = post.title
-    document.getElementById('post-body').innerHTML = post.body
+    document.getElementById('post-h').innerHTML = post.title
+    document.getElementById('post-b').innerHTML = post.body
 }
 
 renderSingle()
-
-(function () {
-  let postSingle = localStorage.getItem('viewedPost')
-  let post = JSON.parse(postSingle)
-  console.log(post)
-
-  document.getElementById('post-id').innerHTML = post.id
-  document.getElementById('post-title').innerHTML = post.title
-  document.getElementById('post-body').innerHTML = post.body
-})();
